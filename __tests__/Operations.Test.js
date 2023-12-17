9ae619df67cbce5a9f9a8c0f4ae712568181e403
Wone11 <gelayegeremew@gmail.com>
2023-12-17 22:19:25 +0300

    operations test senarios added

--- __tests__/Operations.Test.js
+++ __tests__/Operations.Test.js
@@ -1,16 +1,37 @@
 const Operations = require("../Operations");
 const Addition = require("../Sum");
-const operation = new Operations(12, 50);
-
+const operation = new Operations(12, 0);
 
 test('adds 50 + 12 to  equal 62', () => {
     expect(Addition(50, 12)).toBe(62);
 })
 
 test('additional operation', () => {
-    expect(operation.Add()).toBe(62);
-})
+    expect(operation.Add()).toBe(12);
+});
 
-test('MultiplicationOperatios ', () => {
-    expect(operation.Multiply()).toBe(600)
-})
+test('Multiplication Operatios ', () => {
+    expect(operation.Multiply()).toEqual(0)
+});
+
+test('Devision Operations', () => {
+    expect(operation.Devide()).toBe('Zero devision Error Occured');
+});
+
+test('five square equals twenty five', () => {
+    expect(5 * 5).toBe(25);
+});
+
+test('Object assignment', () => {
+    let data = { one: 1 }
+    data['two'] = 2;
+    expect(data).toEqual({ one: 1, two: 2 });
+});
+
+//test one for null value 
+test('null and zero value data to be falsy', () => {
+    let data = null;
+    let zeroData = 0;
+    expect(data).toBeFalsy();
+    expect(zeroData).toBeFalsy();
+});
