4c7d12bb955eeb0ab329c94b7d11f2ed874ac012
Wone11 <gelayegeremew@gmail.com>
2023-12-19 00:02:33 +0300

    mocking a return value and spying to spy restore test done for methods

--- __tests__/Mock.Test.js
+++ __tests__/Mock.Test.js
@@ -5,3 +5,18 @@ test('Mock implimentations', () => {
     expect(mock(1)).toBe(51);
     expect(mock(-50)).toBe(0);
 })
+
+/**
+ * Spying on the return value 
+ */
+test('Spying on the objejct', () => {
+    const Video = {
+        Play() {
+            return true;
+        },
+    };
+    const spy = jest.spyOn(Video, 'Play');
+    Video.Play();
+    expect(spy).toHaveBeenCalled();
+    spy.mockRestore();
+})
